2c737df18ae7a89a1999b6b6974bb60f8754a197
Aaron Robson <AaronRobson@users.noreply.github.com>
2019-03-26 23:55:56 +0000

    remove dead code from product module

--- product.py
+++ product.py
@@ -6,10 +6,3 @@ def product(items):
     '''The multiplicative equivalent of the built-in "sum" function.
     '''
     return reduce(mul, items, 1)
-
-
-if __name__ == "__main__":
-    print('Product of empty list is {}.'.format(product(1)))
-    print(
-        'Product of list of 1-5 inclusive is {}.'.
-        format(product(range(1, 4+1))))
